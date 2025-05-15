e3e3271223cd600c32180cb87b5bc782c64bfe39
AdvisorOfNeutronStars <159212503+AdvisorOfNeutronStars@users.noreply.github.com>
2025-05-14 23:33:24 -0400

    Create config.js

---
+++ multibot/ultimate-discord-bot/src/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    token: '', // replace with your discord bots token
+    prefix: '>', // replace with your desired prefix
+    slashCommandPrefix: '/', // debug for future slash command aliases
+};
