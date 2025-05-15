b3040cc4eb57e3eec7a18e78170dc7d61b68918e
AdvisorOfNeutronStars <159212503+AdvisorOfNeutronStars@users.noreply.github.com>
2025-05-14 23:37:57 -0400

    Update utility.js

--- multibot/ultimate-discord-bot/src/commands/utility.js
+++ multibot/ultimate-discord-bot/src/commands/utility.js
@@ -13,13 +13,13 @@ const utilityCommands = {
             .setColor('#0099ff')
             .setTitle('Bot Information')
             .setDescription('This bot is designed to assist with moderation and utility commands.')
-            .addFields(
+            .addFields([
                 { name: 'Author', value: '.folfy7#0', inline: true },
                 { name: 'Version', value: '1.0.0', inline: true },
                 { name: 'GitHub', value: '[GitHub Link](<>)', inline: true },
                 { name: 'Invite Link', value: '[Invite Link](<>)', inline: true },
                 { name: 'Support Server', value: '[Support Server Link](<>)', inline: true }
-            )
+            ])
             .setTimestamp()
             .setFooter({ text: 'Bot Information', iconURL: message.client.user.displayAvatarURL() });
         await message.channel.send({ embeds: [infoEmbed] });
